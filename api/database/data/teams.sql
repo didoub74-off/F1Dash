0e7c51876f4ce96f75202acf4e50260c15940cea
darlannbanache <darlann@banache.fr>
2024-12-10 10:29:32 +0100

    Add Static Data to SQL (drivers and teams)

---
+++ api/database/data/teams.sql
@@ -0,0 +1,11 @@
+INSERT INTO teams (id, name, nationality) VALUES
+    ('red_bull', 'Red Bull Racing', 'Austrian'),
+    ('mclaren', 'McLaren F1 Team', 'British'),
+    ('ferrari', 'Scuderia Ferrari', 'Italian'),
+    ('mercedes', 'Mercedes Formula 1 Team', 'German'),
+    ('alpine', 'Alpine F1 Team', 'French'),
+    ('williams', 'Williams Racing', 'British'),
+    ('aston_martin', 'Aston Martin Formula 1 Team', 'British'),
+    ('haas', 'Haas F1 Team', 'American'),
+    ('rb', 'RB F1 Team', 'Italian'),
+    ('sauber', 'Kick Sauber F1 Team', 'Swiss')
